1b0a489ef92c575e6e0214e46772b903c30d2e1a
luis.eduardo <luisedu.unifal@gmail.com>
2021-08-04 18:18:35 -0300

    Working with Routers

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -1,8 +1,12 @@
+import { CourseInfoComponent } from './courses/course-info.component';
+import { Exception404Component } from './exception404/exception-404.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { CourseListComponent } from './courses/course-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { StarsComponent } from './stars/stars.component';
 import { AppComponent } from './app.component';
 
@@ -11,11 +15,28 @@ import { AppComponent } from './app.component';
     AppComponent,
     CourseListComponent,
     StarsComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent,
+    Exception404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: '**', component: Exception404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ course-manager/src/app/exception404/exception-404.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  templateUrl: './exception-404.component.html'
+})
+
+export class Exception404Component {
+
+}
